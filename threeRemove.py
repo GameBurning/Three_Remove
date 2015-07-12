11e6e0903c01d3d70bdcb46f55d43e4bbe4ae7f9
Ginta Berry <i@gaoboning.com>
2015-07-12 23:23:45 +0800

    1.2
    A little problem

--- threeRemove.py
+++ threeRemove.py
@@ -1,25 +1,27 @@
 __author__ = 'gaoboning'
 import random
 
-#Store the same letters
-class group():
-    def __init__(self):
-        self.letter = ['0']
-        self.number = 0
-        self.list = [(0,0)]
-    def appendLetter(self, l_in):
-        self.letter = l_in
-    def appendList(self, l_in):
-        self.list.append(l_in)
-    def printOut(self):
-        print "\n------------------\nLetter:"+self.letter+"\nList:"+self.list+"\n"
-
 letters = ['A','B','C','D']
-diamondTable = []
+#diamondTable = []
+diamondTable =  [['C', 'D', 'D', 'B', 'B', 'C', 'A', 'B', 'B', 'A'],
+                 ['A', 'C', 'B', 'C', 'D', 'B', 'A', 'B', 'B', 'B'],
+                 ['D', 'B', 'C', 'D', 'B', 'A', 'A', 'C', 'B', 'A'],
+                 ['D', 'D', 'C', 'D', 'D', 'A', 'A', 'A', 'A', 'A'],
+                 ['A', 'D', 'A', 'D', 'C', 'D', 'D', 'B', 'A', 'A'],
+                 ['A', 'D', 'B', 'C', 'C', 'D', 'B', 'D', 'C', 'C'],
+                 ['C', 'D', 'A', 'A', 'B', 'A', 'A', 'A', 'C', 'C'],
+                 ['D', 'B', 'D', 'A', 'C', 'D', 'C', 'A', 'C', 'C'],
+                 ['A', 'D', 'C', 'A', 'B', 'B', 'C', 'D', 'D', 'D'],
+                 ['D', 'B', 'C', 'A', 'A', 'D', 'B', 'B', 'D', 'A']]
+resultTable = [] #result of horizon match
 
-resultTable_Hori = [] #result of horizon match
-resultTable_Vert = [] #result of vertical match
-#global N #rows and lines
+# diamondTable = [
+#     [0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0],
+# ]
 
 
 def initTable(n):
@@ -27,70 +29,93 @@ def initTable(n):
     N = n
     for i in range(0, n):
         tmp = []
-        zerosHori = []
-        zerosVert = []
+        zeros = []
         for j in range(0, n):
             tmp.append(letters[random.randint(0,3)])
-            zerosHori.append(0)
-            zerosVert.append(0)
+            zeros.append(0)
         diamondTable.append(tmp)
-        resultTable_Hori.append(zerosHori)
-        resultTable_Vert.append(zerosVert)
-    #for i in range(0, n):
-    #    print Diamond_table[i][:]
+        resultTable.append(zeros)
+
 
 def printTable(table):
     for i in range(0, len(table)):
-        for j in range(0, len(table)):
+        for j in range(0, len(table[0])):
             print table[i][j],
         print '\n',
 
 
 
-def detectTable():
-    number = 1
+def detectTable(setlist):
+    N = 10
     if(len(diamondTable) == 0):
         print "Table not initial successfully!"
     else:
-        for i in range(0, N-2):
-            for j in range(0, N-2):
+        for i in range(0, N):
+            for j in range(0, N):
                 #detect the horizon match
-                if(resultTable_Hori[i][j] == 0):
+                if j < N - 2:
                     (a, b, c) = (diamondTable[i][j], diamondTable[i][j+1], diamondTable[i][j+2])
                     if a == b and b == c:
-                        resultTable_Hori[i][j] = (number, a)
-                        resultTable_Hori[i][j+1] = (number, a)
-                        resultTable_Hori[i][j+2] = (number, a)
-                        for t in range(j+3, N-2):
+                        matchset = set([(i,j),(i,j+1),(i,j+2)])
+                        for t in range(j+3, N):
                             if diamondTable[i][t] == a:
-                                resultTable_Hori[i][t] = (number, a)
+                                matchset.add((i,t))
                             else:
                                 break
-                        number += 1
 
+                        # repeat = 0
+                        # for w in setlist:
+                        #     if matchset & w != set([]):
+                        #         setlist.remove(w)
+                        #         matchset = matchset | w
+                        #
+                        #         repeat = 1
+                        # if repeat == 0:
+                        #     setlist.append(matchset)
+                        removeRepeat(matchset, setlist)
 
                 #detect the vertical match
-                '''use even number to store horizon match and odd number to store vertical match'''
-                if(resultTable_Vert[i][j] == 0):
+
+                if i < N - 2:
                     (a, b, c) = (diamondTable[i][j], diamondTable[i+1][j], diamondTable[i+2][j])
                     if a == b and b == c:
-                        resultTable_Vert[i][j] = (number, a)
-                        resultTable_Vert[i+1][j] = (number, b)
-                        resultTable_Vert[i+2][j] = (number, c)
-                        for t in range(i+3, N-2):
+                        matchset = set([(i,j),(i+1,j),(i+2,j)])
+                        for t in range(i+3, N):
                             if diamondTable[t][j] == a:
-                                resultTable_Vert[t][j] = (number, a)
+                                matchset.add((t,j))
                             else:
                                 break
-                        number += 1
+
+                        # repeat = 0
+                        # for w in setlist:
+                        #     if matchset & w != set([]):
+                        #         setlist.append(w | matchset)
+                        #         setlist.remove(w)
+                        #         repeat = 1
+                        # if repeat == 0:
+                        #     setlist.append(matchset)
+                        removeRepeat(matchset, setlist)
+
+def removeRepeat(set_i, list_i):
+    repeat = 0
+    for w in list_i:
+        if set_i & w != set([]):
+            set_i |= w
+            list_i.remove(w)
+            repeat = 1
+        if(removeRepeat(set_i,list_i) == 0):
+            list_i.append(set_i)
+    return repeat
 
 
 
-initTable(10)
+#initTable(10)
 print"\n-----------Diam-----------\n"
-printTable(diamondTable)
-detectTable()
-print"\n-----------Hori-----------\n"
-printTable(resultTable_Hori)
-print"\n-----------Vert-----------\n"
-printTable(resultTable_Vert)
+printTable( diamondTable)
+set_temp = []
+detectTable(set_temp)
+for w in set_temp:
+    print w,
+    position = w.pop()
+    print diamondTable[position[0]][position[1]]
+
